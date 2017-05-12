f11c662965f204b7f79084d4c8738c5512b5b253
Ruben Carreira <ruben_carreira97@hotmail.com>
2017-05-12 17:53:28 +0100

    correcao de erros em vistas

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -14,9 +14,50 @@
         </div>
         <div class="column">
             <div class="box">
-
-                @include('perfil.helper')
+                <div class="media-content">
+                    <div class="content">
+                        <p><strong>{{ auth()->user()->name }}</strong><!-- <small>{{ auth()->user()->email }}</small>--></p>
+                        <p>{{ auth()->user()->presentation }}</p>
+                    </div>
+                </div>
                 
+                <div class="columns">
+                    <div class="column is-half">
+                        <a class="has-text-left">
+                            <span class="icon">
+                                <i class="fa fa-phone"></i>
+                            </span>
+                            {{ auth()->user()->phone ? auth()->user()->phone : 'Não há contacto' }}
+                        </a>
+                    </div>
+                    <div class="column is-half">
+                        <a class="has-text-lef">
+                            <span class="icon">
+                                <i class="fa fa-link"></i>
+                            </span>
+                            {{ auth()->user()->phone ? auth()->user()->profile_url : 'Não há ligação para um perfil externo' }}
+                        </a>
+                    </div>
+                </div>
+
+                <div class="columns">
+                    <div class="column is-half">
+                        <a class="has-text-left">
+                            <span class="icon">
+                                <i class="fa fa-envelope"></i>
+                            </span>
+                            {{ auth()->user()->email }}
+                        </a>
+                    </div>
+                    <div class="column is-half">
+                        <a class="has-text-left">
+                            <span class="icon">
+                                <i class="fa fa-building-o"></i>
+                            </span>
+                            {{ auth()->user()->departament->name }}
+                        </a>
+                    </div>
+                </div>
                 <nav class="level is-mobile">
                     <div class="level-right">
                         <a href="{{ route('perfil.edit') }}" class="level-item button is-info">
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -18,15 +18,54 @@
 	                </div>
 	            </div>
 	        </div>
-
 	        <div class="column">
 	            <div class="box">
-	            
-	                @include('perfil.helper')
+	                <div class="media-content">
+	                    <div class="content">
+	                        <p ><strong>{{ $user->name }}</strong></p>
+	                        <p>{{ $user->presentation }}</p>
+	                    </div>
+	                </div>
+	                <div class="columns">
+	                    <div class="column is-half">
+	                        <a class="has-text-left">
+	                            <span class="icon">
+	                                <i class="fa fa-phone"></i>
+	                            </span>
+		                            {{ $user->phone ? $user->phone : 'Não há contacto' }}
+		                    </a>
+	                    </div>
+	                    <div class="column is-half">
+	                        <a class="has-text-lef">
+	                            <span class="icon">
+	                                <i class="fa fa-link"></i>
+	                            </span>
+		                            {{ $user->phone ? $user->profile_url : 'Não há ligação para um perfil externo' }}
+		                    </a>
+	                    </div>
+                	</div>
 
-	                <div class="level-left">
+                	<div class="columns">
+                    	<div class="column is-half">
+                        	<a class="has-text-left">
+                            	<span class="icon">
+                                	<i class="fa fa-envelope"></i>
+                            	</span>
+	                            	{{ $user->phone ? $user->phone : 'Não há email' }}
+	                        </a>
+                    	</div>
+                    	<div class="column is-half">
+                        	<a class="has-text-left">
+                            	<span class="icon">
+                             	   <i class="fa fa-building-o"></i>
+                            	</span>
+								{{ $user->departament->name }}
+                            	</a>
+                    	</div>
+                	</div>
+                	<div class="level-left">
 	                	<a class="level-item button is-primary">Voltar</a>
-                    	@unless(auth()->check() && auth()->user()->isAdmin())
+	                    @unless(auth()->check() && auth()->user()->isAdmin())
                         <form action="{{ route('user.change') }}" method="post" >
                         	{{ csrf_field() }}
                         	{{ method_field('PUT') }}
@@ -35,8 +74,7 @@
 								{{ $user->blocked == 1 ? 'Desbloquear' : 'Bloquear' }}
 							</button>
                         </form>
-                    	@endunless
-	                	
+	                    @endunless	                
 	                </div>
 	            </div>
 	        </div>
